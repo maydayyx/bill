b92547c86a0432ff68358dec17f9797dd29ac9f2
YeXing_0901 <13319139772@163.com>
2024-06-18 12:58:16 +0800

    完成主题定制

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import router from "@/router/index.jsx";
 import {RouterProvider} from 'react-router-dom'
-import './index.css'
-
+import '@/index.css'
+import '@/theme.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
    <RouterProvider router={router} />
 )
--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,4 +1,5 @@
 import {Link, Outlet} from "react-router-dom";
+import {Button} from "antd-mobile";
 
 const Layout = () => {
     return (
@@ -7,6 +8,10 @@ const Layout = () => {
             <Link to='/'>月度</Link>
             <Link to='/year'>年度</Link>
         <Outlet/>
+            <Button color='primary'>测试</Button>
+            <div className='purple-theme'>
+                <Button color='primary'>局部</Button>
+            </div>
         </div>
     )
 }
