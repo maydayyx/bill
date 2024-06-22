5337ee1f6675347b86a119d3fd74ed08ad22ebc7
YeXing_0901 <13319139772@163.com>
2024-06-22 08:20:23 +0800

    月度账单按月分组

--- src/pages/Month/index.jsx
+++ src/pages/Month/index.jsx
@@ -1,6 +1,77 @@
+import {NavBar, DatePicker} from 'antd-mobile'
+import './index.scss'
+import {useMemo, useState} from "react";
+import classNames from "classnames";
+import dayjs from "dayjs";
+import {useSelector} from "react-redux";
+import _ from 'lodash'
+
+
 const Month = () => {
+    // 按月分组数据
+    const billList = useSelector(state=>state.bill.billList)
+    const monthGroup = useMemo(() => {
+        return _.groupBy(billList,(item)=>dayjs(item.date).format('YYYY-MM'))
+    }, [billList]);
+
+    // 控制弹窗的打开关闭
+    const [dateVisible, setDateVisible] = useState(false)
+
+    // 控制时间显示
+    const [currnetDate,setCurrentDate] = useState(()=>{
+        return dayjs( new Date()).format('YYYY-MM')
+    })
+
+    //时间选择器确认
+    const onConfirm = (time) => {
+        setDateVisible(false)
+        //把选中的时间赋值给currentDate
+        const formatDate = dayjs(time).format('YYYY-MM')
+        setCurrentDate(formatDate)
+    }
     return (
-        <div>I am Month</div>
+        <div className="monthlyBill">
+            <NavBar className="nav" backArrow={false}>
+                月度收支
+            </NavBar>
+            <div className="content">
+                <div className="header">
+                    {/* 时间切换区域 */}
+                    <div className="date" onClick={() => setDateVisible(true)}>
+            <span className="text">
+             {currnetDate.toString()} 月账单
+            </span>
+                        <span className={classNames('arrow',dateVisible && 'expand')}></span>
+                    </div>
+                    {/* 统计区域 */}
+                    <div className='twoLineOverview'>
+                        <div className="item">
+                            <span className="money">{100}</span>
+                            <span className="type">支出</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{200}</span>
+                            <span className="type">收入</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{200}</span>
+                            <span className="type">结余</span>
+                        </div>
+                    </div>
+                    {/* 时间选择器 */}
+                    <DatePicker
+                        className="kaDate"
+                        title="记账日期"
+                        precision="month"
+                        visible={dateVisible}
+                        max={new Date()}
+                        onCancel={() => setDateVisible(false)}
+                        onConfirm={onConfirm}
+                        onClose={() => setDateVisible(false)}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
