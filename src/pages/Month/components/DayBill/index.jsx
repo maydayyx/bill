0ca55c5d52f1ee5c0ae8db9f6eddadbf59360a7d
YeXing_0901 <13319139772@163.com>
2024-06-23 12:35:31 +0800

    控制展开和收起

--- src/pages/Month/components/DayBill/index.jsx
+++ src/pages/Month/components/DayBill/index.jsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames'
 import './index.scss'
 import  {billTypeToName} from '@/contants/index.js'
-import {useMemo} from "react";
+import {useMemo, useState} from "react";
 
 const DailyBill = ({date, billList}) => {
 
@@ -15,12 +15,14 @@ const DailyBill = ({date, billList}) => {
             }
         }
         , [billList])
+    // 控制折叠
+    const [visible,setVisible] = useState(false)
     return (
         <div className={classNames('dailyBill')}>
             <div className="header">
                 <div className="dateIcon">
                     <span className="date">{date}</span>
-                    <span className={classNames('arrow')}></span>
+                    <span className={classNames(  'arrow',visible && 'expand')} onClick={()=>setVisible(!visible)}></span>
                 </div>
                 <div className="oneLineOverview">
                     <div className="pay">
@@ -38,7 +40,7 @@ const DailyBill = ({date, billList}) => {
                 </div>
             </div>
             {/* 单日列表 */}
-            <div className="billList">
+            <div className="billList" style={{display:visible?'block':'none'}}>
                 {billList.map(item => {
                     return (
                         <div className="bill" key={item.id}>
